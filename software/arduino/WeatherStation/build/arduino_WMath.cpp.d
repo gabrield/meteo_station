981c0ae90a9e87713de9d47b248606736e8e6282
Gabriel Duarte <confusosk8@gmail.com>
2014-09-03 14:53:14 -0300

    reorganization

--- software/arduino/WeatherStation/build/arduino_WMath.cpp.d
+++
@@ -1,2 +0,0 @@
-/home/gduarte/sketchbook/WeatherStation/build/arduino_WMath.cpp.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/WMath.cpp
--- software/arduino/WeatherStation/build/arduino_new.cpp.d
+++
@@ -1,3 +0,0 @@
-/home/gduarte/sketchbook/WeatherStation/build/arduino_new.cpp.o: \
- /usr/share/arduino/hardware/arduino/cores/arduino/new.cpp \
- /usr/share/arduino/hardware/arduino/cores/arduino/new.h
