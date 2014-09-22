1d9b9e8293b72898d93efcfb8c7f0eb5ae6848aa
Gabriel Duarte <confusosk8@gmail.com>
2014-09-22 20:37:13 -0300

    libthingspeak skeleton

---
+++ software/linux/libthingspeak/src/thingspeak.c
@@ -0,0 +1,50 @@
+#include <thingspeak.h>
+
+
+ts_context_t *ts_create_context(char *api_key, ts_feed_id_t feed_id)
+{
+	ts_context_t *ctx = (ts_context_t*)malloc(sizeof(ts_context_t));
+
+	ctx->api_key = api_key;
+	ctx->feed_id = feed_id;
+
+	return ctx;
+}
+
+
+void inline ts_delete_context(ts_context_t *ctx)
+{
+	if(ctx)
+		free(ctx);
+}
+
+
+
+ts_datapoint_t *ts_set_value_i32(ts_datapoint_t *datapoint, int32_t val)
+{
+	datapoint->value_type = TS_VALUE_TYPE_I32;
+	datapoint->value.i32_value = val;
+}
+
+
+int32_t ts_get_value_i32(ts_datapoint_t *datapoint)
+{
+	return datapoint->value.i32_value;
+}
+
+
+ts_datapoint_t *ts_set_value_f32(ts_datapoint_t *datapoint, float val)
+{
+	datapoint->value_type = TS_VALUE_TYPE_F32;
+	datapoint->value.f32_value = val;
+}
+
+float ts_get_value_f32(ts_datapoint_t *datapoint)
+{
+	return datapoint->value.f32_value;
+}
+
+
+
+
+
